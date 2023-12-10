91b916194aa98c788bf8fb9999edc8effd667c73
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-12-10 19:06:06 -0300

    adjusting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-tertiary text-white p-2 text-center fixed bottom-0 w-full">
-      &copy; 2023 langdom | Todos os direitos reservados
+      <p className="text-sm sm:text-md md:text-lg lg:text-xl xl:text-2xl">
+        &copy; 2023 langdom | Todos os direitos reservados
+      </p>
     </footer>
   );
 }
